81b22e90fbf3db9ececa440de9056a2fd1654507
Dmytro Didukh <jeriko.outback@gmail.com>
2022-07-02 14:59:43 +0300

    setup linters

--- src/containers/app/app.tsx
+++ src/containers/app/app.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 
 import styles from './app.module.scss';
 
-export const App: React.FC = () => {
-    console.log(styles);
-
-    return <div>hello</div>;
+export const App: React.FC = (): JSX.Element => {
+    return <div className={styles.root}> hello</div>;
 };
